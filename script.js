31fcd8b76dd650d62cf74fa55287f6cd78a9c043
Dane Williamson <danezellwilliamson@gmail.com>
2022-03-24 17:33:00 -0500

    rock paper scissors array creation

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// function that randomly returns the word rock, paper, or scissors
+function computerPlay () {
+  // creates an array that holds the 3 options; rock, paper, scissors
+  let myArray = ["rock", "paper", "scissors"];
+  // chooses a option from the array at random
+      
+}
