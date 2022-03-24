3d64d72709285185ab11dde04f67f4f8a379cf27
Dane Williamson <danezellwilliamson@gmail.com>
2022-03-24 18:02:47 -0500

    computerPlay function complete

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function computerPlay () {
   // creates an array that holds the 3 options; rock, paper, scissors
   let myArray = ["rock", "paper", "scissors"];
   // chooses a option from the array at random
-      
+  // Math.floor rounds to the nearest whole number
+  // Math.random gives a random # between 0-1. Multiplied by the length of my
+  // array so it now gives numbers between 0 and 2
+  let randomChoice = Math.floor((Math.random() * myArray.length));
+  // returns an array value that corrosponds with the random number choice
+  return myArray[randomChoice];
 }
